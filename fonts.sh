482e1ff7d1d86673696ece66872f52a213373e4f
al3xk <ak80mail@gmail.com>
2023-11-07 13:22:28 +0100

    Update fonts.sh
    loop through fonts

--- fonts.sh
+++ fonts.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 username=$(id -u -n 1000)
 
+fonts=("CascadiaCode" "FiraCode" "Ubuntu" "JetBrainsMono")
+
 sudo apt install fonts-font-awesome -y
-# Cascadia NF
-wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip"
-unzip CascadiaCode.zip -d /home/$username/.fonts
-rm CascadiaCode.zip
 
-# FiraCode NF
-wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip"
-unzip FiraCode.zip -d /home/$username/.fonts
-rm FiraCode.zip
-
-# Ubuntu NF
-wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Ubuntu.zip"
-unzip Ubuntu.zip -d /home/$username/.fonts
-rm Ubuntu.zip
-
-# JetBrains
-wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip"
-unzip JetBrainsMono.zip -d /home/$username/.fonts
-rm JetBrainsMono.zip
+for font in "${fonts[@]}"; do
+    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/${font}.zip"
+    unzip "${font}.zip" -d "/home/$username/.fonts"
+    rm "${font}.zip"
+done
 
 fc-cache -vf
