edcc40708a792cc28a4cd86f434b233f69d2923a
al3xk <ak80mail@gmail.com>
2023-11-07 13:45:57 +0100

    Update default_apps.sh

--- default_apps.sh
+++ default_apps.sh
@@ -4,4 +4,11 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install curl wget htop ncdu kitty tldr neofetch timeshift synaptic -y
+# Create folders in user directory (eg. Documents,Downloads,etc.)
+xdg-user-dirs-update
+
+# basic apps
+sudo apt install curl wget htop ncdu kitty tldr neofetch timeshift synaptic build-essential make gcc unzip gedit -y
+
+# additional apps
+sudo apt install gnome-software autojump 
