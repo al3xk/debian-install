baebc6662643e33e5639dc32b6b0cc253bb860de
al3xk <ak80mail@gmail.com>
2023-11-14 11:36:42 +0100

    apps changed

--- default_apps.sh
+++ default_apps.sh
@@ -8,7 +8,7 @@ sudo apt upgrade -y
 xdg-user-dirs-update
 
 # basic apps
-sudo apt install curl vim wget htop ncdu kitty tldr neofetch timeshift build-essential make gcc unzip -y
+sudo apt install curl vim wget htop ncdu kitty tldr neofetch timeshift build-essential make gcc unzip exa -y
 
 # additional apps
 sudo apt install gnome-software autojump synaptic gedit
