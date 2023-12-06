e9cce14d9a9e5db2d5f8c3337475f596bcb7fba1
al3xk <ak80mail@gmail.com>
2023-12-06 10:38:38 +0100

    Update ufw.sh
    added ufw basic config

--- scripts/ufw.sh
+++ scripts/ufw.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+sudo apt install ufw -y
+
+# set default policies
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+# sudo ufw allow from [IP|SUBNET] to any port 22
+# sudo ufw allow 443
