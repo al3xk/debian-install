c8a05770f0906d898c39a212623b1443cb678f9f
al3xk <ak80mail@gmail.com>
2024-07-02 10:42:12 +0200

    Update default_apps.sh

--- scripts/default_apps.sh
+++ scripts/default_apps.sh
@@ -5,10 +5,13 @@ sudo apt update
 sudo apt upgrade -y
 
 # Create folders in user directory (eg. Documents,Downloads,etc.)
-xdg-user-dirs-update
+# xdg-user-dirs-update
 
 # basic apps
-sudo apt install curl vim wget htop ncdu kitty tldr neofetch timeshift build-essential make gcc unzip exa -y
+sudo apt install curl vim wget htop ncdu kitty tldr neofetch timeshift build-essential make gcc unzip -y
 
 # additional apps
 sudo apt install autojump synaptic gedit
+
+# gnome extras
+sudo apt install gnome-shell-extension-manager gnome-tweaks
