b812edb65378ef2e20c062213f7f43897fd2f66b
al3xk <ak80mail@gmail.com>
2026-01-22 20:53:06 +0100

    Create nvim

---
+++ dotconfig/nvim/init.lua/lua/options.lua
@@ -0,0 +1,19 @@
+---
+--- Allgemeine Einstellungen
+---
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.signcolumn = "yes"
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.undofile = true
+opt.termguicolors = true
+
+---
+--- Suchen
+---
+opt.ignorecase = true
+opt.laststatus = 3
