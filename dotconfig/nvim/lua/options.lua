1245db2d1ba4a6048a3e90acc4f2f28dd30c156e
al3xk <ak80mail@gmail.com>
2026-01-22 21:23:13 +0100

    Update options.lua

--- dotconfig/nvim/lua/options.lua
+++ dotconfig/nvim/lua/options.lua
@@ -6,7 +6,11 @@ local opt = vim.opt
 opt.number = true
 opt.relativenumber = true
 opt.cursorline = true
+opt.wrap = false
 opt.signcolumn = "yes"
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
 opt.mouse = "a"
 opt.clipboard = "unnamedplus"
 opt.undofile = true
@@ -16,4 +20,24 @@ opt.termguicolors = true
 --- Suchen
 ---
 opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
 opt.laststatus = 3
+
+---
+--- Einrückung Tabs
+---
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.smartindent = true
+
+--- 
+--- Dateien und Backup
+---
+opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
+opt.confirm = true
